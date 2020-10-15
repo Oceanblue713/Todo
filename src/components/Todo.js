add370d207c222be18f98cfd3a9bae7b9437e641
Anri Liy <amedamachip713@gmail.com>
2020-10-14 19:12:49 -0700

    Handle Status

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -10,7 +10,7 @@ const Todo = ( {text, todo, todos, setTodos} ) => {
     setTodos(todos.map((item) => {
       if(item.id === todo.id) {
         return {
-          ...item, completed: !item.completed
+          ...item, completed: !item.completed,
         }
       }
       return item;
@@ -19,8 +19,8 @@ const Todo = ( {text, todo, todos, setTodos} ) => {
 
   return (
     <div className="todo">
-      <li className='todo-item'>{text}</li>
-      <button className="complete-btn">
+      <li className={`todo-item ${todo.completed ? "completed" : ''}`}>{text}</li>
+      <button onClick={completeHandler} className="complete-btn">
         <i className='fas fa-check'></i>
       </button>
       <button onClick={deleteHandler} className="trash-btn">
